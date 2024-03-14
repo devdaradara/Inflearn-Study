0fb90e127728e02aea8891ea4c723bddd714b002
daradara <devdaradara@gmail.com>
2024-03-14 21:49:49 +0900

    ch03

---
+++ Inflearn Java/java-start/src/variable/Var7.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // 불리언
+        char d = 'A'; // 문자 하나
+        String e = "Hello Java"; // 문자열
+    }
+}
