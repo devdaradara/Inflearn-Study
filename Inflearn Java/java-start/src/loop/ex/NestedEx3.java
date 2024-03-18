5cffe1a6f2179cd8a9b93d2f0eb71a389699c7cf
daradara <devdaradara@gmail.com>
2024-03-18 13:08:48 +0900

    ch 05

---
+++ Inflearn Java/java-start/src/loop/ex/NestedEx3.java
@@ -0,0 +1,17 @@
+package loop.ex;
+
+public class NestedEx3 {
+    public static void main(String[] args) {
+        int num = 4;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 0; j < num - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
