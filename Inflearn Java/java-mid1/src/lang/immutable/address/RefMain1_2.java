8b376d8ff4880b4f34ee6c24ca193edea90adab3
daradara <devdaradara@gmail.com>
2024-04-21 00:54:07 +0900

    [java-mid-1] ch02

---
+++ Inflearn Java/java-mid1/src/lang/immutable/address/RefMain1_2.java
@@ -0,0 +1,16 @@
+package lang.immutable.address;
+
+public class RefMain1_2 {
+    public static void main(String[] args) {
+        Address a = new Address("서울");
+        Address b = new Address("서울");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("b: 부산으로 변경");
+        b.setValue("부산");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
