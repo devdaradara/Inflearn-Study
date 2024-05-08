34b547ba450e7c5ad8f7a5a4fd176ef133b5c8bc
devdaradara <devdaradara@gmail.com>
2024-05-08 20:07:21 +0900

    [java-mid2] init

--- Inflearn Spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ Inflearn Spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
